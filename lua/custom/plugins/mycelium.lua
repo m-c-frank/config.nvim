5e0f8f4728005cdb24a30cbfb85771e2e819d08f
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-12-24 12:16:56 +0100

    using exposed config

--- lua/custom/plugins/mycelium.lua
+++ lua/custom/plugins/mycelium.lua
@@ -1,6 +1,7 @@
 return {
   'm-c-frank/mycelium.nvim',
   config = function()
-    require("mycelium")
+    local mycelium = require("mycelium")
+    mycelium.config.generate_url="http://192.168.2.177:11434/api/generate"
   end,
 }
